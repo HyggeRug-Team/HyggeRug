620c22c8a482b780aff47341b63239aeb58e9aad
Cabradeosas <hectorcastrocascales5@gmail.com>
2026-01-27 12:32:29 +0100

    Mejora del header para que solo salga el logo en las paginas de inicio de sesion, ademas de crear la pagina de login(Sin terminar)

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 "use client";
 import React, { useState, useEffect } from 'react';
+// Necesario para las rutas
+import { usePathname } from 'next/navigation';
 import Logo from '../Logo/Logo'
 import styles from "./Header.module.css";
 //Importación de los iconos usados
@@ -20,6 +22,12 @@ import CuteMessage from "../CuteMessage/CuteMessage";
 function Header() {
   // Estado por defecto en cerrado
   const [isMenuOpen, setIsOpenMenu] = useState(false);
+
+  // Ruta actual
+  const pathname = usePathname();
+  // Si está en alguna de esas rutas que son de incio de sesión o algo parecido
+  const isAuthPage = pathname === '/auth' || pathname === '/login' || pathname === '/register';
+
   // Función para alternar entre estados
   const toggleMenu = () => {
     setIsOpenMenu(!isMenuOpen)
@@ -53,59 +61,64 @@ function Header() {
         <Logo />
         <h1>Hygge Rug</h1>
       </div>
-      <nav>
-        <a href='#'>Tienda</a>
-        <a href='#'>Personalizar</a>
-        <a href='#'>Nosotros</a>
-      </nav>
-      <div className={styles.buttonsMenu}>
-        <button><CiSearch size={35} /></button>
-        <button><CiShoppingCart size={35} /></button>
-        <button><MdAccountCircle size={35} /></button>
-      </div>
-      {/*Cuando se pulse el estado del menu cambia*/}
-      <div className={styles.hamburgerButton} onClick={toggleMenu}>
-        {isMenuOpen ? <IoMdClose size={35} /> : <RxHamburgerMenu size={35} />}
-      </div>
-      {/*Si el estado is open es true se asigna la clase mobileMenuActive además de la clase principal*/}
-      <div className={`${styles.mobileMenu} ${isMenuOpen ? styles.mobileMenuActive : ""}`}>
+      {/*Si la ruta no es ninguna de las del isAuthPage va a dibujar todo el header, si no simplemente saldrá el logo*/}
+      {!isAuthPage && (
+        <>
+          <nav>
+            <a href='#'>Tienda</a>
+            <a href='#'>Personalizar</a>
+            <a href='#'>Nosotros</a>
+          </nav>
+          <div className={styles.buttonsMenu}>
+            <button><CiSearch size={35} /></button>
+            <button><CiShoppingCart size={35} /></button>
+            <button><MdAccountCircle size={35} /></button>
+          </div>
+          {/*Cuando se pulse el estado del menu cambia*/}
+          <div className={styles.hamburgerButton} onClick={toggleMenu}>
+            {isMenuOpen ? <IoMdClose size={35} /> : <RxHamburgerMenu size={35} />}
+          </div>
+          {/*Si el estado is open es true se asigna la clase mobileMenuActive además de la clase principal*/}
+          <div className={`${styles.mobileMenu} ${isMenuOpen ? styles.mobileMenuActive : ""}`}>
 
-        <div className={styles.headerMobileMenu}>
-          <h2>Hygge Rug</h2>
-          <h4>Diseño artesanal para tu hogar</h4>
-        </div>
+            <div className={styles.headerMobileMenu}>
+              <h2>Hygge Rug</h2>
+              <h4>Diseño artesanal para tu hogar</h4>
+            </div>
 
-        {/*Botones del menú desplegable*/}
-        <div className={styles.buttonMobileMenu}>
-          {MENU_ITEMS.map((item) => (
-            <a
-              key={item.id}
-              href={item.url}
-              className={styles.menuButton}
-              onClick={() => setIsOpenMenu(false)}
-            >
-              <div className={styles.iconMobilMenu}>
-                {item.icon}
-              </div>
-              <span>{item.label}</span>
-            </a>
-          ))}
-        </div>
-        <div className={styles.cuteMessage}>
-          <CuteMessage Icon={FaRegHeart} text="Hecho a mano directo a tu corazón" />
-        </div>
-        <div className={styles.socialMedias}>
-          <a href="#" target="_blank" rel="noopener noreferrer">
-            <FaFacebookF />
-          </a>
-          <a href="https://www.instagram.com/hygge_rug/" target="_blank" rel="noopener noreferrer">
-            <AiFillInstagram />
-          </a>
-          <a href="#" target="_blank" rel="noopener noreferrer">
-            <FaXTwitter />
-          </a>
-        </div>
-      </div>
+            {/*Botones del menú desplegable*/}
+            <div className={styles.buttonMobileMenu}>
+              {MENU_ITEMS.map((item) => (
+                <a
+                  key={item.id}
+                  href={item.url}
+                  className={styles.menuButton}
+                  onClick={() => setIsOpenMenu(false)}
+                >
+                  <div className={styles.iconMobilMenu}>
+                    {item.icon}
+                  </div>
+                  <span>{item.label}</span>
+                </a>
+              ))}
+            </div>
+            <div className={styles.cuteMessage}>
+              <CuteMessage Icon={FaRegHeart} text="Hecho a mano directo a tu corazón" />
+            </div>
+            <div className={styles.socialMedias}>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <FaFacebookF />
+              </a>
+              <a href="https://www.instagram.com/hygge_rug/" target="_blank" rel="noopener noreferrer">
+                <AiFillInstagram />
+              </a>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <FaXTwitter />
+              </a>
+            </div>
+          </div>
+        </>
+      )}
     </header>
   )
 }
---
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './LoginForm.module.css'
+function LoginForm() {
+  return (
+    <form>
+        <labe>
+            <input type='text'></input>
+            <input type='email'></input>
+            <input type='password'></input>
+        </label>
+    </form>
+  )
+}
+
+export default LoginForm
